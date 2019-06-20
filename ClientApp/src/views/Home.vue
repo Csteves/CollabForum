8f3ff404f7af6abcd6d90cfecb88cd3ad0be23f3
Craig Stevens <craig.stevens@egov.com>
2019-06-20 11:37:57 -0600

    Update README, Landing

--- ClientApp/src/views/Home.vue
+++ ClientApp/src/views/Home.vue
@@ -7,7 +7,7 @@
     <v-slide-y-transition mode="out-in">
       <v-layout column>        
         <h1 class="headline">Collaboration Portal</h1>
-        <p>Welcome to the Collaboration Portal, bringing you a forum for all of your Q&A's and giving you a feed to stay up to date. </p>
+        <p>Welcome to the Collaboration Portal, bringing to you a forum for all of your Q&A's, and giving you a feed to stay up to date. </p>
 
       </v-layout>
     </v-slide-y-transition>
